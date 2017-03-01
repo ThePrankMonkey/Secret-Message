29a87a38fbc056a5c7b8df1785dca8cf42443a33
Matthew Hellmer <ThePrankMonkey@gmail.com>
2017-03-01 12:10:08 -0500

    Fixed for python3 and other errors

--- SecretMessage.py
+++ SecretMessage.py
@@ -1,7 +1,8 @@
 #input
-s = raw_input('Please enter a message: ')
-#s = 'abcdefghijklmnopqrstuvwxyz'
-print
+#s = raw_input('Please enter a message: ')
+s = 'abcdefghijklmnopqrstuvwxyz' #Uncoded
+s = 'XzXyXxXwXvXuXtXsXrXqXpXoXnXmXlXkXjXiXhXgXfXeXdXcXbXa' #Coded
+print(s)
 
 #decode - will check to see if the message is coded.
 def coded(s):
@@ -9,15 +10,14 @@ def coded(s):
         if i != 'X':
             return False
     return True
+    
 if coded(s):
-    print 'Here is the decoded message: '
-    r = t[::-2]
-    print r 
-
-#encode
+    print('Here is the decoded message: ')
+    r = s[::-2]
+    print(r)
 else:
-    print 'The encoded message follows:'
+    print('The encoded message follows:')
     t = ''
     for i in s[::-1]:
         t += 'X' + i
-    print t
+    print(t)
